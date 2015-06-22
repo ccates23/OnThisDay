22af240b95d95b620686705be8a18bde226a3499
chadcates <cates.chad@gmail.com>
2015-06-22 15:53:06 -0500

    made changes to files

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,23 @@
 var cors = 'http://crossorigin.me/';
 var date = new Date();
-var day = date.getDay();
-var month = date.getMonth();
-var API_URL = 'http://www.api.hiztory.org/events/' + month + '/' + day + '/1/15/api.xml'
+var day = date.getDate();
+var month = date.getMonth() + 1;
+var API_URL = 'http://www.api.hiztory.org/events/' + month + '/' + day + '/1/15/api.xml';
 
 $.get(cors + API_URL, function(data){
 	console.log(data);
-});
+
+	var xml = $.parseXML(data)
+	console.log(xml);
+   });
+
+
+
+
+
+
+
+
+
+
 
