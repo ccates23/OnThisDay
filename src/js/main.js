d81a828bf8f9120e94a21f60933e7e8a8e2d7356
chadcates <cates.chad@gmail.com>
2015-06-24 09:20:02 -0500

    made changes to index.jade and main.js

--- src/js/main.js
+++ src/js/main.js
@@ -5,15 +5,56 @@ var cors = 'http://crossorigin.me/';
 var date = new Date();
 var day = date.getDate();
 var month = date.getMonth() + 1;
-var API_URL = 'http://www.api.hiztory.org/events/' + month + '/' + day + '/1/15/api.xml';
+var API_URLevents = 'http://www.api.hiztory.org/events/' + month + '/' + day + '/1/15/api.xml';
+var API_URLbirths = 'http://www.api.hiztory.org/births/' + month + '/' + day + '/1/15/api.xml';
+var API_URLdeaths = 'http://www.api.hiztory.org/deaths/' + month + '/' + day + '/1/15/api.xml';
+var births = [];
+var deaths = [];
+var events = [];
 
-$.get(cors + API_URL, function(data){
-	console.log(data);
+$.get(cors + API_URLevents, function(res){
 
-	var xml = $.parseXML(data);
-    var json = xml2json(xml);
-	console.log(json);
+	var xml = $.parseXML(res);
+    $(xml).find('event').each(function () {
+    	events.push({
+    		date: $(this).attr('date'),
+    		content: $(this).attr('content')
+        });
+    });
+console.log(events);
+
+
 });
+
+$.get(cors + API_URLbirths, function(res){
+	
+	var xml = $.parseXML(res);
+    $(xml).find('event').each(function () {
+    	births.push({
+    		date: $(this).attr('date'),
+    		content: $(this).attr('content')
+        });
+    });
+
+console.log(births);
+});
+
+$.get(cors + API_URLdeaths, function(res){
+	
+	var xml = $.parseXML(res);
+    $(xml).find('event').each(function () {
+    	deaths.push({
+    		date: $(this).attr('date'),
+    		content: $(this).attr('content')
+        });
+
+    });
+
+console.log(deaths);
+});
+
+
+
 
 
 
